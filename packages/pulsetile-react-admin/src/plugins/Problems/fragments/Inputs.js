3d31642f47c5847e0c6dc829eb6d5da147007460
kevinoflanagan <kevinoflanagan@rcsi.ie>
2019-09-27 09:06:20 +0100

    problem async select input

--- packages/pulsetile-react-admin/src/plugins/Problems/fragments/Inputs.js
+++ packages/pulsetile-react-admin/src/plugins/Problems/fragments/Inputs.js
@@ -5,7 +5,18 @@ import moment from "moment";
 import { withStyles } from '@material-ui/core/styles';
 import FormGroup from "@material-ui/core/FormGroup";
 
+import AsyncSelect from 'react-select/lib/Async';
+import PropTypes from 'prop-types';
+import _ from "lodash";
+
+import ProbInput from './ProbInput'
+
 import formStyles from "../../../config/formStyles";
+import { Field } from 'redux-form';
+
+import { emphasize, makeStyles, useTheme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
 
 /**
  * This component returns inputs for Problems creation/editing forms
@@ -14,17 +25,97 @@ import formStyles from "../../../config/formStyles";
  * @param {shape} classes
  * @param {shape} rest
  */
+
+
+
+export const stateOptions = [
+  { value: 'AL', label: 'Alabama' },
+  { value: 'AK', label: 'Alaska' },
+  { value: 'AS', label: 'American Samoa' },
+  { value: 'AZ', label: 'Arizona' },
+  { value: 'AR', label: 'Arkansas' },
+  { value: 'CA', label: 'California' },
+  { value: 'CO', label: 'Colorado' },
+  { value: 'CT', label: 'Connecticut' },
+  { value: 'DE', label: 'Delaware' },
+  { value: 'DC', label: 'District Of Columbia' },
+  { value: 'FM', label: 'Federated States Of Micronesia' },
+  { value: 'FL', label: 'Florida' },
+  { value: 'GA', label: 'Georgia' },
+  { value: 'GU', label: 'Guam' },
+  { value: 'HI', label: 'Hawaii' },
+  { value: 'ID', label: 'Idaho' },
+  { value: 'IL', label: 'Illinois' },
+  { value: 'IN', label: 'Indiana' },
+  { value: 'IA', label: 'Iowa' },
+  { value: 'KS', label: 'Kansas' },
+  { value: 'KY', label: 'Kentucky' },
+  { value: 'LA', label: 'Louisiana' },
+  { value: 'ME', label: 'Maine' },
+  { value: 'MH', label: 'Marshall Islands' },
+  { value: 'MD', label: 'Maryland' },
+  { value: 'MA', label: 'Massachusetts' },
+  { value: 'MI', label: 'Michigan' },
+  { value: 'MN', label: 'Minnesota' },
+  { value: 'MS', label: 'Mississippi' },
+  { value: 'MO', label: 'Missouri' },
+  { value: 'MT', label: 'Montana' },
+  { value: 'NE', label: 'Nebraska' },
+  { value: 'NV', label: 'Nevada' },
+  { value: 'NH', label: 'New Hampshire' },
+  { value: 'NJ', label: 'New Jersey' },
+  { value: 'NM', label: 'New Mexico' },
+  { value: 'NY', label: 'New York' },
+  { value: 'NC', label: 'North Carolina' },
+  { value: 'ND', label: 'North Dakota' },
+  { value: 'MP', label: 'Northern Mariana Islands' },
+  { value: 'OH', label: 'Ohio' },
+  { value: 'OK', label: 'Oklahoma' },
+  { value: 'OR', label: 'Oregon' },
+  { value: 'PW', label: 'Palau' },
+  { value: 'PA', label: 'Pennsylvania' },
+  { value: 'PR', label: 'Puerto Rico' },
+  { value: 'RI', label: 'Rhode Island' },
+  { value: 'SC', label: 'South Carolina' },
+  { value: 'SD', label: 'South Dakota' },
+  { value: 'TN', label: 'Tennessee' },
+  { value: 'TX', label: 'Texas' },
+  { value: 'UT', label: 'Utah' },
+  { value: 'VT', label: 'Vermont' },
+  { value: 'VI', label: 'Virgin Islands' },
+  { value: 'VA', label: 'Virginia' },
+  { value: 'WA', label: 'Washington' },
+  { value: 'WV', label: 'West Virginia' },
+  { value: 'WI', label: 'Wisconsin' },
+  { value: 'WY', label: 'Wyoming' },
+];
+
+const getAsyncOptions = (inputValue) => {
+    return new Promise((resolve, reject) => {
+      const filtered = _.filter(stateOptions, o =>
+        _.startsWith(_.toLower(o.label), _.toLower(inputValue))
+      );
+      resolve(filtered.slice(0, 3));
+    });
+  }
+
+const ProblemsInput = () => (
+    <span>
+        <Field name="problem" source="problem" component={ProbInput}/>
+    </span>
+);
+
+
 const ProblemsInputs = ({ classes, ...rest }) => (
     <React.Fragment>
 
-        <FormGroup className={classes.formGroup}>
-            <TextInput
-                source="problem"
-                label="Problem / Issue"
-                fullWidth
-                InputProps={{ disableUnderline: true, classes: { root: classes.customRoot, input: classes.customInput } }}
+	<FormGroup className={classes.formGroup}>
+	    <ProblemsInput 
+	        source="problem" 
+		label= "Problem / Issue"
+		InputProps={{ disableUnderline: true, classes: { root: classes.customRoot, input: classes.customTextarea } }}
                 InputLabelProps={{ shrink: true, className: classes.customFormLabel }}
-            />
+	    />
         </FormGroup>
 
         <FormGroup className={classes.formGroup}>
@@ -96,3 +187,5 @@ const ProblemsInputs = ({ classes, ...rest }) => (
 );
 
 export default withStyles(formStyles)(ProblemsInputs);
+
+
