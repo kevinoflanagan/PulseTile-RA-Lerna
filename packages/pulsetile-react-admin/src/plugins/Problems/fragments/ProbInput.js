1341335bb486ed75fc77b40fc3c2f6a851b7a88e
kevinoflanagan <kevinoflanagan@rcsi.ie>
2019-10-02 12:25:36 +0100

    fetch request finished

--- packages/pulsetile-react-admin/src/plugins/Problems/fragments/ProbInput.js
+++ packages/pulsetile-react-admin/src/plugins/Problems/fragments/ProbInput.js
@@ -1,106 +1,64 @@
 import React, { Component } from "react";
 import AsyncSelect from "react-select/lib/Async";
-import _ from "lodash";
-
-export const stateOptions = [
-  { value: 'AL', label: 'Alabama' },
-  { value: 'AK', label: 'Alaska' },
-  { value: 'AS', label: 'American Samoa' },
-  { value: 'AZ', label: 'Arizona' },
-  { value: 'AR', label: 'Arkansas' },
-  { value: 'CA', label: 'California' },
-  { value: 'CO', label: 'Colorado' },
-  { value: 'CT', label: 'Connecticut' },
-  { value: 'DE', label: 'Delaware' },
-  { value: 'DC', label: 'District Of Columbia' },
-  { value: 'FM', label: 'Federated States Of Micronesia' },
-  { value: 'FL', label: 'Florida' },
-  { value: 'GA', label: 'Georgia' },
-  { value: 'GU', label: 'Guam' },
-  { value: 'HI', label: 'Hawaii' },
-  { value: 'ID', label: 'Idaho' },
-  { value: 'IL', label: 'Illinois' },
-  { value: 'IN', label: 'Indiana' },
-  { value: 'IA', label: 'Iowa' },
-  { value: 'KS', label: 'Kansas' },
-  { value: 'KY', label: 'Kentucky' },
-  { value: 'LA', label: 'Louisiana' },
-  { value: 'ME', label: 'Maine' },
-  { value: 'MH', label: 'Marshall Islands' },
-  { value: 'MD', label: 'Maryland' },
-  { value: 'MA', label: 'Massachusetts' },
-  { value: 'MI', label: 'Michigan' },
-  { value: 'MN', label: 'Minnesota' },
-  { value: 'MS', label: 'Mississippi' },
-  { value: 'MO', label: 'Missouri' },
-  { value: 'MT', label: 'Montana' },
-  { value: 'NE', label: 'Nebraska' },
-  { value: 'NV', label: 'Nevada' },
-  { value: 'NH', label: 'New Hampshire' },
-  { value: 'NJ', label: 'New Jersey' },
-  { value: 'NM', label: 'New Mexico' },
-  { value: 'NY', label: 'New York' },
-  { value: 'NC', label: 'North Carolina' },
-  { value: 'ND', label: 'North Dakota' },
-  { value: 'MP', label: 'Northern Mariana Islands' },
-  { value: 'OH', label: 'Ohio' },
-  { value: 'OK', label: 'Oklahoma' },
-  { value: 'OR', label: 'Oregon' },
-  { value: 'PW', label: 'Palau' },
-  { value: 'PA', label: 'Pennsylvania' },
-  { value: 'PR', label: 'Puerto Rico' },
-  { value: 'RI', label: 'Rhode Island' },
-  { value: 'SC', label: 'South Carolina' },
-  { value: 'SD', label: 'South Dakota' },
-  { value: 'TN', label: 'Tennessee' },
-  { value: 'TX', label: 'Texas' },
-  { value: 'UT', label: 'Utah' },
-  { value: 'VT', label: 'Vermont' },
-  { value: 'VI', label: 'Virgin Islands' },
-  { value: 'VA', label: 'Virginia' },
-  { value: 'WA', label: 'Washington' },
-  { value: 'WV', label: 'West Virginia' },
-  { value: 'WI', label: 'Wisconsin' },
-  { value: 'WY', label: 'Wyoming' },
-];
-
-function getElasticSearch() {
-	const myHeaders = new Headers();
-
-	myHeaders.append('Content-Type', 'application/json');
-	myHeaders.append('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1lZGljYXRpb25fY2F0YWxvZ0B0ZXN0LmNvbSIsIl9pZCI6IjVkOGIzMTA2N2VhNDBjNTM2NDQ4MTY1OCIsImlhdCI6MTU2OTQ4ODQ0NywiZXhwIjoxNjAxMDI0NDQ3fQ.nzvZ999pcagZbp2OfzrIWqS8fDkxeYm3rYsefO4YZDI');
-
-	return fetch('https://cpcr01.tcd.ie/snomedapi/api/v1/medication_catalog/search/elastic/ipu_vtm/paracetamol', {
-	  method: 'GET',
-	  headers: myHeaders,
-	})
-	.then(response => {
-	    if (response.status === 200) {
-	      console.log(response);
-	      console.log("Testing response log");
-	      return response.json();
-	    } else {
-	      throw new Error('Something went wrong on api server!');
-	    }
-	  })
-	  .then(response => {
-	    console.debug(response);
-	  }).catch(error => {
-	    console.error(error);
-	  });
-}
-
+//import { stateOptions } from "./docs/data";
 
 export default class SingleSelect extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { stateOptions: [] };
+  }
+
   getAsyncOptions(inputValue) {
-    getElasticSearch();
+    console.log(inputValue);
+    console.log(this.state);
+    const myHeaders = new Headers();
+    let url = `https://cpcr01.tcd.ie/snomedapi/api/v1/medication_catalog/search/elastic/ipu_vtm/${inputValue}`;
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append(
+      "Authorization",
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1lZGljYXRpb25fY2F0YWxvZ0B0ZXN0LmNvbSIsIl9pZCI6IjVkOGIzMTA2N2VhNDBjNTM2NDQ4MTY1OCIsImlhdCI6MTU2OTQ4ODQ0NywiZXhwIjoxNjAxMDI0NDQ3fQ.nzvZ999pcagZbp2OfzrIWqS8fDkxeYm3rYsefO4YZDI"
+    );
+
+    fetch(url, {
+      method: "GET",
+      headers: myHeaders
+    }).then(response => {
+      if (response.status === 200) {
+        response.json().then(data => {
+          if (data.hits !== undefined) {
+            const snomedItems = data.hits.hits.map(element => {
+              return {
+                value: element._id,
+                label: element._source.NM
+              };
+            });
+            this.setState(
+              {
+                stateOptions: snomedItems
+              },
+              function() {
+                // setState is asynchronous! This function gets called
+                // when it's finished.
+                console.log("Job's done");
+                console.log(this.state);
+                console.log(snomedItems);
+              }
+            );
+          }
+        });
+      }
+    });
+
     return new Promise((resolve, reject) => {
-      const filtered = _.filter(stateOptions, o =>
-        _.startsWith(_.toLower(o.label), _.toLower(inputValue))
-      );
-      resolve(filtered.slice(0, 3));
+      //const filtered = _.filter(this.state.stateOptions, o =>
+      //  _.startsWith(_.toLower(o.label), _.toLower(inputValue))
+      //);
+      //resolve(this.state.stateOptions);
+      setTimeout(() => resolve(this.state.stateOptions), 300);
     });
   }
+
+  componentDidUpdate() {}
 
   render() {
     return (
@@ -111,11 +69,10 @@ export default class SingleSelect extends Component {
           isClearable
           className="basic-single"
           classNamePrefix="select"
-	  name="problem"
+          name="search"
           loadOptions={inputValue => this.getAsyncOptions(inputValue)}
         />
       </div>
     );
   }
 }
-
